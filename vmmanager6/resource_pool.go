f4b81618cbb5379102cfdb31c7e5cc12ddfa65d7
Ilya Kalinichenko <i.kalinichenko@ispsystem.com>
2022-04-27 22:55:31 +0800

    adding pool resource

---
+++ vmmanager6/resource_pool.go
@@ -0,0 +1,144 @@
+package vmmanager6
+
+import (
+	"context"
+//	"strings"
+	"log"
+//	"strconv"
+//	"fmt"
+	"encoding/json"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+        "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	vm6api "github.com/usaafko/vmmanager6-api-go"
+//        "github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+// using a global variable here so that we have an internally accessible
+// way to look into our own resource definition. Useful for dynamically doing typecasts
+// so that we can print (debug) our ResourceData constructs
+var poolResource *schema.Resource
+
+func resourcePool() *schema.Resource {
+        poolResource = &schema.Resource{
+                Create:        resourcePoolCreate,
+                Read:          resourcePoolRead,
+                UpdateContext: resourcePoolUpdate,
+                Delete:        resourcePoolDelete,
+                Importer: &schema.ResourceImporter{
+                        StateContext: schema.ImportStatePassthroughContext,
+                },
+		Schema: map[string]*schema.Schema{
+			"pool": {
+				Type:        schema.TypeString,
+                                Required:    true,
+                                Description: "Ipv4 or Ipv6 Network in CIDR format",
+			},
+			"ranges": {
+                                Type:     schema.TypeList,
+                                Required:    true,
+                                Description: "Range of ips in pool. Format: 192.168.0.1 or 192.168.0.1-192.168.0.10 or 192.168.0.0/24",
+                                Elem: &schema.Resource{
+                                	Schema: map[string]*schema.Schema{
+                                		"range": {
+                                			Type: schema.TypeString,
+                                			Required: true,
+                                		}
+                                }
+                        },
+			"desc": {
+                                Type:     schema.TypeString,
+                                Optional: true,
+                                Description: "Pool Description",
+                                Default: "",
+                        },
+
+
+		},
+		}
+        return poolResource
+}
+
+func resourcePoolCreate(d *schema.ResourceData, meta interface{}) error {
+	// create a logger for this function
+        logger, _ := CreateSubLogger("resource_pool_create")
+
+	// DEBUG print out the create request
+        flatValue, _ := resourceDataToFlatValues(d, poolResource)
+        jsonString, _ := json.Marshal(flatValue)
+
+	pconf := meta.(*providerConfiguration)
+        lock := pmParallelBegin(pconf)
+        //defer lock.unlock()
+        client := pconf.Client
+
+	config := vm6api.ConfigNewPool{
+                Name:         d.Get("pool").(string),
+                Note:         d.Get("desc").(string),
+                Ranges:       d.Get("ranges").([]map[string]string)
+	}
+	vmid, err := config.CreatePool(client)
+	if err != nil {
+		return err
+	}
+	d.SetId(vmid)
+	logger.Debug().Msgf("Finished Pool read resulting in data: '%+v'", string(jsonString))
+	
+	log.Print("[DEBUG][PoolCreate] vm creation done!")
+        lock.unlock()
+        return nil
+}
+
+func resourcePoolUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourcePoolRead(d *schema.ResourceData, meta interface{}) error {
+	return _resourcePoolRead(d, meta)
+}
+
+func resourcePoolDelete(d *schema.ResourceData, meta interface{}) error {
+	pconf := meta.(*providerConfiguration)
+        lock := pmParallelBegin(pconf)
+        defer lock.unlock()
+
+        client := pconf.Client
+	err := client.DeletePool(d.Id())
+	return err
+
+}
+
+func _resourcePoolRead(d *schema.ResourceData, meta interface{}) error {
+	pconf := meta.(*providerConfiguration)
+        lock := pmParallelBegin(pconf)
+        defer lock.unlock()
+        client := pconf.Client
+        // create a logger for this function
+        logger, _ := CreateSubLogger("resource_pool_read")
+
+	// Try to get information on the Pool. If this call err's out
+        // that indicates the Pool does not exist. We indicate that to terraform
+        // by calling a SetId("")
+	_, err := client.GetPoolInfo(d.Id())
+	if err != nil {
+                d.SetId("")
+                return nil
+        }
+        config, err := vm6api.NewConfigPoolFromApi(d.Id(), client)
+        if err != nil {
+                return err
+        }
+
+
+        logger.Debug().Msgf("[READ] Received Pool Config from VMmanager6 API: %+v", config)
+
+	d.Set("pool", config.Name)
+	d.Set("desc", config.Note)
+	
+	// DEBUG print out the read result
+        flatValue, _ := resourceDataToFlatValues(d, poolResource)
+        jsonString, _ := json.Marshal(flatValue)
+	logger.Debug().Msgf("Finished VM read resulting in data: '%+v'", string(jsonString))
+
+        return nil
+}
+
